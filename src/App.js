0daf57db8d95592d9ca8c10edeffea210349a156
Arsenalfutbol14 <mczanaty@gmail.com>
2021-12-10 15:32:09 -0500

    Fixedd blog post to get rid of repeated header, and footer along with Boilpoint code

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import Api from "./components/API call/Api"
 import './App.css';
 // import Card from "./components/card/card"
 import Blog from "./components/card/Blog"
+import Header from "./components/Header/Navbar"
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
+        <Header />
         < Api />
         < Blog />
+        <Footer />
     </div>
   );
 }
--- src/components/card/Blog.jsx
+++ src/components/card/Blog.jsx
@@ -13,8 +13,8 @@ import {
 } from "reactstrap";
 
 // core components
-import DangerNavbar from "../Header/Navbar";
-import FooterGray from "../Footer/Footer";
+// import DangerNavbar from "../Header/Navbar";
+// import FooterGray from "../Footer/Footer";
 
 function BlogPosts(props) {
     const { _id, title, body, imgURL } = props;
@@ -29,7 +29,7 @@ function BlogPosts(props) {
     });
     return (
         <>
-            <DangerNavbar />
+            {/*<DangerNavbar />*/}
             <div className="wrapper">
                 <div className="main">
                     <div className="section section-white">
@@ -48,25 +48,21 @@ function BlogPosts(props) {
                                         <Card className="card-blog card-plain text-center">
                                             <div className="card-image">
                                                 <a href="#pablo" onClick={(e) => e.preventDefault()}>
-                                                    />
+                                                    {imgURL}
                                                 </a>
                                             </div>
                                             <CardBody>
                                                 <div className="card-category">
-                                                    <Badge className="main-tag" color="primary">
-                                                        Featured
-                                                    </Badge>
+
                                                 </div>
                                                 <a href="javascrip: void(0);">
                                                     <CardTitle tag="h3">
                                                         {title}
-
                                                     </CardTitle>
                                                 </a>
                                                 <div className="card-description">
                                                     <p>
                                                         {body}
-
                                                     </p>
                                                 </div>
                                             </CardBody>
@@ -77,76 +73,12 @@ function BlogPosts(props) {
                                     </Col>
                                 </Row>
                             </div>
-                            <hr />
-                            <br />
-                            <br />
-                            <div className="article">
-                                <Row>
-                                    <Col className="ml-auto mr-auto" md="8">
-                                        <Card className="card-blog card-plain text-center">
-                                            <div className="card-image">
-                                                <a href="#pablo" onClick={(e) => e.preventDefault()}>
-
-                                                    />
-                                                </a>
-                                            </div>
-
-                                            <Button className="btn-round" color="danger" size="sm">
-                                                Read more
-                                            </Button>
-                                        </Card>
-                                    </Col>
-                                </Row>
-                            </div>
-                            <hr />
-                            <br />
-                            <br />
-                            <div className="article">
-                                <Col className="ml-auto mr-auto" md="8">
-                                    <Card className="card-blog card-plain text-center">
-                                        <div className="card-image">
-                                            <a href="#pablo" onClick={(e) => e.preventDefault()}>
-
-                                                />
-                                                <p className="image-thumb">Photo by Cam Adams</p>
-                                            </a>
-                                        </div>
-
-                                        <Button className="btn-round" color="danger" size="sm">
-                                            Read more
-                                        </Button>
-                                    </Card>
-                                </Col>
-                            </div>
-                            <hr />
-                            <Row>
-                                <Col md="12">
-                                    <div className="pull-left">
-                                        <Button
-                                            className="btn-link btn-move-left"
-                                            color="default"
-                                            size="sm"
-                                        >
-                                            <i className="fa fa-angle-left mr-1" />
-                                            Older Posts
-                                        </Button>
-                                    </div>
-                                    <div className="pull-right">
-                                        <Button
-                                            className="btn-link btn-move-right"
-                                            color="default"
-                                            size="sm"
-                                        >
-                                            Newer Posts <i className="fa fa-angle-right" />
-                                        </Button>
-                                    </div>
-                                </Col>
-                            </Row>
+                        <br/>
                         </Container>
                     </div>
                 </div>
             </div>
-            <FooterGray />
+            {/*<FooterGray />*/}
         </>
     );
 }
